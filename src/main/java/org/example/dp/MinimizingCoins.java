22af3218a493832d1f0a15226dde459b8ba35e39
nouran3abdelaal <nourakhaled329@gmail.com>
2024-09-25 00:18:10 +0300

    Minimizing Coins p2

--- src/main/java/org/example/dp/MinimizingCoins.java
+++ src/main/java/org/example/dp/MinimizingCoins.java
@@ -3,9 +3,14 @@ package org.example.dp;
 import java.util.Scanner;
 
 public class MinimizingCoins {
+    static int [] memo;
+
     public static int helper(int[] values, int sum) {
         if(sum==0) {
             return 0;
+        }
+        if(memo[sum]!=0) {
+            return memo[sum];
         }
         int minVal= Integer.MAX_VALUE;
         int tmp=0;
@@ -13,12 +18,17 @@ public class MinimizingCoins {
             if(sum-val>=0) {
                 tmp=1+helper(values,sum-val);
                 minVal=Math.min(tmp, minVal);
+                memo[sum]=minVal;
             }
         }
 
-        return minVal==Integer.MAX_VALUE || minVal== 0?-1:minVal;
-    }
-        public static void main(String[] args) {
+
+        memo[sum]=minVal==Integer.MAX_VALUE || minVal== 0?-1:minVal;
+        return memo[sum];
+}
+
+
+    public static void main(String[] args) {
             // Create a Scanner object to read user input
             Scanner input = new Scanner(System.in);
             int n = input.nextInt();
@@ -27,7 +37,7 @@ public class MinimizingCoins {
             for(int i=0; i<n; i++){
                 values[i]= input.nextInt();
             }
-
+            memo= new int[sum+1];
             input.close();
             System.out.print(helper(values,sum));
         }
