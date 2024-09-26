90fc441dea92d2eeac6598bc0daa907f2fdbf318
nouran3abdelaal <nourakhaled329@gmail.com>
2024-09-26 21:37:28 +0300

    Grid Paths  p3

--- src/main/java/org/example/dp/GridPaths.java
+++ src/main/java/org/example/dp/GridPaths.java
@@ -1,43 +1,58 @@
 package org.example.dp;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GridPaths {
     int n;
-    String[][] matrix;
+    char[][] matrix;
+    int [][] memo;
 
-    public GridPaths(int n, String[][] matrix) {
+
+    public GridPaths(int n, char[][] matrix) {
         this.n = n;
         this.matrix = matrix;
+        this.memo = new int [n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(memo[i],-1);
+        }
     }
     private int solve() {
         return solveHelper(0,0);
     }
 
     private int solveHelper(int i, int i1) {
-        if(i==n-1 && i1==n-1){
+        if(i==n-1 && i1==n-1 && !(matrix[i][i1] == '*')){
             return 1;
         }
-        if(i+1<n && !matrix[i+1][i1].equals("*")){
-            if( i1+1<n && !matrix[i][i1+1].equals("*")){
-                return solveHelper(i,i1+1) +solveHelper(i+1,i1);
+        if(memo[i][i1]!=-1){
+            return memo[i][i1];
+        }
+        if(i+1<n && !(matrix[i+1][i1] == '*')){
+            if( i1+1<n && !(matrix[i][i1+1] == '*')){
+                 memo[i][i1] = solveHelper(i,i1+1) +solveHelper(i+1,i1);
+                return memo[i][i1];
             }
-            return solveHelper(i+1,i1);
+            memo[i][i1] =  solveHelper(i+1,i1);
+            return memo[i][i1];
         }
-        if( i1+1<n && !matrix[i][i1+1].equals("*")) {
-            return solveHelper(i, i1 + 1);
+        if( i1+1<n && !(matrix[i][i1+1] == '*')) {
+            memo[i][i1] =  solveHelper(i, i1 + 1);
+            return memo[i][i1];
         }
-        return  0;
+        memo[i][i1] =0;
+        return  memo[i][i1];
     }
 
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        String [][] matrix = new String[n][n];
+        char [][] matrix = new char[n][n];
         for(int i=0; i<n;i ++){
+            String li = input.next();
             for(int j=0; j<n;j++)
-             matrix[i][j] = input.next();
+             matrix[i][j] = li.charAt(j);
         }
         GridPaths gridPaths = new GridPaths(n,matrix);
         int result = gridPaths.solve();
