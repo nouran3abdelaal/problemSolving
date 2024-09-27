62974523b587769ae6aaf2be789fb4cb071d430d
nouran3abdelaal <nourakhaled329@gmail.com>
2024-09-27 18:38:56 +0300

    Where is the Ghost p2

--- src/main/java/org/example/dp/WhereistheGhost.java
+++ src/main/java/org/example/dp/WhereistheGhost.java
@@ -1,22 +1,31 @@
 package org.example.dp;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class WhereistheGhost {
     int n;
+    int [][] memo;
     public WhereistheGhost(int n) {
         this.n = n;
+        this.memo = new int[n+1][2520];
+        for (int i=1;i<=n+1;i++){
+            Arrays.fill(memo[i-1],-1);
+        }
     }
 
-    public  int dp(int i,int val) {
-        if(val%2520==0) {
+    public int dp(int i,int val) {
+        if(val==0) {
             return 1;
         }
         if(i>n) {
             return 0;
         }
-
-        return dp(i+1, val)+dp(i+1,val*i);
+        if(memo[i][val]!=-1){
+            return memo[i][val];
+        }
+        memo[i][val] = dp(i+1, val)+dp(i+1,(val*i)%2520);
+        return memo[i][val];
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
