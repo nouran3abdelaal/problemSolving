a435f721fa28bd4aabefb9547d57f6aefaf09872
nouran3abdelaal <nourakhaled329@gmail.com>
2024-11-09 20:28:24 +0200

    wayTooLongWords

---
+++ src/main/java/org/example/pupils_plan/way_too_long_words.java
@@ -0,0 +1,35 @@
+package org.example.pupils_plan;
+
+import java.util.Scanner;
+
+public class way_too_long_words {
+    static String[] texts;
+
+    public way_too_long_words(String[] texts) {
+        this.texts = texts;
+    }
+    private void solveOneText(String text) {
+        if(text.length()<10 ){
+            System.out.println(text);
+            return;
+        }
+        int size = text.length()-2;
+        System.out.println(text.charAt(0)+""+size+text.charAt(text.length()-1));
+    }
+    private void solve() {
+        for(String text: texts){
+            solveOneText(text);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        String [] inputs = new String[n];
+        for(int i=0;i<n;i++){
+            inputs[i] = input.next();
+        }
+        way_too_long_words wayTooLongWords = new way_too_long_words(inputs);
+        input.close();
+        wayTooLongWords.solve();
+    }
+}
